912717872f9389798f64e3f84eb2f76e33b5c41d
zch0429 <13131456@me.mcu.edu.tw>
2026-06-28 21:29:51 +0800

    [負責人6] 完成購物車功能優化與商品編輯修復

--- OnlineShop/Models/CartItem.cs
+++ OnlineShop/Models/CartItem.cs
@@ -6,6 +6,10 @@
         public string ProductName { get; set; }
         public decimal Price { get; set; }
         public int Quantity { get; set; }
+
+        
+        public int Stock { get; set; }
+
         public decimal SubTotal => Price * Quantity;
     }
 }
