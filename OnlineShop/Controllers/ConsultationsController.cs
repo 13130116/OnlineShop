f18004b189e5a3de1984f56551f394180d2552b8
chenshinchien <13130932@me.mcu.edu.tw>
2026-07-09 01:17:46 +0800

    完成 線上諮詢留言系統 。

---
+++ OnlineShop/Controllers/ConsultationsController.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using OnlineShop.Data;
+using OnlineShop.Models;
+using Microsoft.AspNetCore.Authorization;
+
+namespace OnlineShop.Controllers
+{
+    public class ConsultationsController : Controller
+    {
+        private readonly OnlineShopContext _context;
+
+        public ConsultationsController(OnlineShopContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Consultations
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Consultations.ToListAsync());
+        }
+
+        // GET: Consultations/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var consultation = await _context.Consultations
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (consultation == null)
+            {
+                return NotFound();
+            }
+
+            return View(consultation);
+        }
+
+        // GET: Consultations/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Consultations/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Question")] Consultation consultation)
+        {
+            if (ModelState.IsValid)
+            {
+                consultation.UserId = User.Identity?.Name ?? "Guest";
+                consultation.CreatedAt = DateTime.Now;
+                consultation.IsReplied = false;
+                consultation.Answer = "";
+
+                _context.Add(consultation);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(MyConsultations));
+            }
+            return View(consultation);
+        }
+
+        // GET: Consultations/Edit/5
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var consultation = await _context.Consultations.FindAsync(id);
+            if (consultation == null)
+            {
+                return NotFound();
+            }
+            return View(consultation);
+        }
+
+        // POST: Consultations/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,UserId,Question,Answer,CreatedAt,IsReplied")] Consultation consultation)
+        {
+            if (id != consultation.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    consultation.IsReplied = true;
+
+                    _context.Update(consultation);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ConsultationExists(consultation.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(consultation);
+        }
+
+        // GET: Consultations/Delete/5
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var consultation = await _context.Consultations
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (consultation == null)
+            {
+                return NotFound();
+            }
+
+            return View(consultation);
+        }
+
+        // POST: Consultations/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var consultation = await _context.Consultations.FindAsync(id);
+            if (consultation != null)
+            {
+                _context.Consultations.Remove(consultation);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        // GET: Consultations/MyConsultations
+        [Authorize]
+        public async Task<IActionResult> MyConsultations()
+        {
+            var userId = User.Identity?.Name;
+
+            var consultations = await _context.Consultations
+                .Where(c => c.UserId == userId)
+                .OrderByDescending(c => c.CreatedAt)
+                .ToListAsync();
+
+            return View(consultations);
+        }
+
+        private bool ConsultationExists(int id)
+        {
+            return _context.Consultations.Any(e => e.Id == id);
+        }
+    }
+}
