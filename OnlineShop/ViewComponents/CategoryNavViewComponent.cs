0a56359734168e76974ce062951312e1e6e08fef
13130116 <13130116@me.mcu.edu.tw>
2026-07-14 10:22:00 +0800

    組長:大更新

---
+++ OnlineShop/ViewComponents/CategoryNavViewComponent.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using OnlineShop.Data;
+
+namespace OnlineShop.ViewComponents
+{
+    public class CategoryNavViewComponent : ViewComponent
+    {
+        private readonly OnlineShopContext _context;
+
+        public CategoryNavViewComponent(OnlineShopContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var categories = await _context.Category
+                .OrderBy(c => c.SortOrder)
+                .ToListAsync();
+
+            return View(categories);
+        }
+    }
+}
